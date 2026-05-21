daa4c4ec427a0b632857298ccc0333040f78713d
beckamDev-code <beckamdev@hotmail.com>
2026-05-21 00:50:32 -0500

    git init

---
+++ src/main/java/com/jessi/meat_and_grill/MeatAndGrillApplication.java
@@ -0,0 +1,13 @@
+package com.jessi.meat_and_grill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatAndGrillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatAndGrillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jessi/meat_and_grill/MeatAndGrillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessi.meat_and_grill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatAndGrillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
