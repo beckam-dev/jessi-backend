0469caa0856aea47edf094b14ea82f850e758f97
beckam-dev <beckamdev@hotmail.com>
2026-06-07 03:24:24 -0500

    Creación de la clase BebidaElegida

---
+++ src/main/java/com/jessi/meat_and_grill/model/BebidaElegida.java
@@ -0,0 +1,37 @@
+package com.jessi.meat_and_grill.model;
+
+import java.math.BigDecimal;
+
+public class BebidaElegida {
+
+    private BebidaOpcion bebida;
+    private int cantidad;
+
+    public BebidaElegida(BebidaOpcion bebida, int cantidad) {
+        this.bebida = bebida;
+        this.cantidad = cantidad;
+    }
+
+    public BebidaOpcion getBebida() {
+        return bebida;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public BigDecimal calcularPrecio() {
+        return bebida.getPrecio().multiply(BigDecimal.valueOf(cantidad));
+    }
+
+    public void aumentarCantidad(int cantidad) {
+        this.cantidad += cantidad;
+    }
+
+    public void disminuirCantidad(int cantidad) {
+        if (this.cantidad - cantidad >= 0) {
+            this.cantidad -= cantidad;
+        }
+    }
+
+}
